f951ef3099a6dd0ccb053be11927603e670eb4a6
Armando <armandoml140299@gmail.com>
2022-09-05 22:18:14 -0500

    probando el usuario de git

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola prro";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 968435419;
 
 // Crea una variable booleana:
 const nuevoBool = null;
