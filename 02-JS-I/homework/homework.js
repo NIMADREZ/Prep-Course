bcf38a2e8145f632e9424516dc4c41939f622e7e
Armando <armandoml140299@gmail.com>
2022-09-05 22:44:46 -0500

    mames

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,7 +10,7 @@ const nuevoNum = 968435419;
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
